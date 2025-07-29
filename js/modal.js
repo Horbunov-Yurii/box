2f4e0fdf73b2784cf1acb23e1e2693b607873657
Horbunov Yurii <urijnet@gmail.com>
2025-07-29 15:49:28 +0300

    fix-js

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,37 @@
     document.body.classList.toggle("no-scroll");
   }
 })();
+
+
+const formRef = document.querySelector(".modal__form");
+const backdrop = document.querySelector(".backdrop");
+
+
+formRef.addEventListener("submit", onFormSubmit);
+backdrop.addEventListener("click", onBackdropClick);
+window.addEventListener("keydown", onEscClick)
+
+
+function onFormSubmit (evt){
+  evt.preventDefault()
+
+  const formData = new FormData(evt.currentTarget);
+  formData.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
+  
+}
+
+function onBackdropClick (evt){
+  if(evt.target === evt.currentTarget){
+    backdrop.classList.toggle("is-hidden");
+  }
+}
+
+
+function onEscClick (evt){
+  if((evt.key = "Escape")){
+    backdrop.classList.toggle("is-hidden");
+  };
+    
+}
